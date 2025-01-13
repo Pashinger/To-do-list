9d725b0d0ba1479bb74d760cad0f10b752ea38cd
Pashinger <koob3n@gmail.com>
2025-01-13 23:26:01 +0100

    Modified port settings for Heroku

--- main.py
+++ main.py
@@ -1108,4 +1108,6 @@ def page_not_found(e: Exception) -> tuple[str, int]:
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
